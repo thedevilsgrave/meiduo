40578874f39a751a0cf2d62e40c3c2220219c9d5
delronkung <delron.con@gmail.com>
2018-05-13 12:08:38 +0800

    使用celery发送短信

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -10,6 +10,7 @@ from meiduo_mall.libs.captcha.captcha import captcha
 from . import constants
 from . import serializers
 from meiduo_mall.libs.yuntongxun.sms import CCP
+from celery_tasks.sms.tasks import send_sms_code
 # Create your views here.
 
 
@@ -55,9 +56,11 @@ class SMSCodeView(GenericAPIView):
         pl.execute()
 
         # 发送短信
-        ccp = CCP()
-        time = str(constants.SMS_CODE_REDIS_EXPIRES / 60)
-        ccp.send_template_sms(mobile, [sms_code, time], constants.SMS_CODE_TEMP_ID)
+        # ccp = CCP()
+        # time = str(constants.SMS_CODE_REDIS_EXPIRES / 60)
+        # ccp.send_template_sms(mobile, [sms_code, time], constants.SMS_CODE_TEMP_ID)
+        # 使用celery发布异步任务
+        send_sms_code.delay(mobile, sms_code)
 
         # 返回
         return Response({'message': 'OK'})
