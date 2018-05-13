c96ddc34c2538fad8781b48fcc2b5d594fe7caf7
delronkung <delron.con@gmail.com>
2018-05-13 10:59:16 +0800

    短信验证码前后端实现

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from django_redis import get_redis_connection
 from django.http.response import HttpResponse
+from rest_framework.generics import GenericAPIView
+import random
+from rest_framework.response import Response
 
 from meiduo_mall.libs.captcha.captcha import captcha
 from . import constants
+from . import serializers
+from meiduo_mall.libs.yuntongxun.sms import CCP
 # Create your views here.
 
 
@@ -22,4 +27,51 @@ class ImageCodeView(APIView):
         redis_conn.setex("img_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
 
         return HttpResponse(image, content_type="images/jpg")
+
+
+class SMSCodeView(GenericAPIView):
+    serializer_class = serializers.CheckImageCodeSerialzier
+
+    def get(self, request, mobile):
+        # 校验图片验证码和发送短信的频次
+        # mobile是被放到了类视图对象属性kwargs中
+        serializer = self.get_serializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+
+        # 校验通过
+        # 生成短信验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+
+        # 保存验证码及发送记录
+        redis_conn = get_redis_connection('verify_codes')
+        # redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        # redis_conn.setex('send_flag_%s' % mobile, constants.SEND_SMS_CODE_INTERVAL, 1)
+
+        # 使用redis的pipeline管道一次执行多个命令
+        pl = redis_conn.pipeline()
+        pl.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        pl.setex('send_flag_%s' % mobile, constants.SEND_SMS_CODE_INTERVAL, 1)
+        # 让管道执行命令
+        pl.execute()
+
+        # 发送短信
+        ccp = CCP()
+        time = str(constants.SMS_CODE_REDIS_EXPIRES / 60)
+        ccp.send_template_sms(mobile, [sms_code, time], constants.SMS_CODE_TEMP_ID)
+
+        # 返回
+        return Response({'message': 'OK'})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
