b54fda60ec4cc9e76e97bfe02143122c97b8ece5
thedevilsgrave <1364415162@qq.com>
2019-12-02 09:48:02 +0800

    热销商品显示

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView
+
+from rest_framework_extensions.cache.mixins import ListCacheResponseMixin
+
+from goods.models import SKU
+from goods.serializers import SKUSerializer
+
+
+class HotSKUListView(ListCacheResponseMixin, ListAPIView):
+    """
+    热销商品, 使用缓存扩展
+    """
+    serializer_class = SKUSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return SKU.objects.filter(category_id=category_id, is_launched=True).order_by('-sales')[:3]
+
+
+
+
+
+
+
+
+
