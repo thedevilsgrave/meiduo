5235282bb312c788018bb765e5f1aebb8288ca39
thedevilsgrave <1364415162@qq.com>
2019-12-05 15:23:11 +0800

    不搞了,累

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'advertising.apps.AdvertisingConfig',
     'goods.apps.GoodsConfig',
     'carts.apps.CartsConfig',
+    'orders.apps.OrdersConfig',
     'ckeditor',  # 富文本编辑器
     'ckeditor_uploader',  # 富文本编辑器上传图片模块
     'corsheaders',
